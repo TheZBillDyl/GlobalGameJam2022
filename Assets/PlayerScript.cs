ec289ed94ee123f8505f11abff287f86093cd5b3
djw191 <50028101+djw191@users.noreply.github.com>
2022-01-22 06:02:02 -0600

    Slight Change in player script

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -12,12 +12,12 @@ public class PlayerScript : NetworkBehaviour
 
     void Update()
     {
-        if (!isLocalPlayer) { return; }
+        if (isLocalPlayer) {
+            float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
+            float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
 
-        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
-        float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
-
-        transform.Rotate(0, moveX, 0);
-        transform.Translate(0, 0, moveZ);
+            transform.Rotate(0, moveX, 0);
+            transform.Translate(0, 0, moveZ);
+        }        
     }
 }
