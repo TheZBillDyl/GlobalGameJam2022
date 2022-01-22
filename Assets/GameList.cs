b23147d08364312abfaff53ab3df5bab0fed167d
Dylan Oelke <dylanoelke.business@gmail.com>
2022-01-22 17:04:11 -0600

    Update

--- Assets/GameList.cs
+++ Assets/GameList.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
 using Mirror;
 using TMPro;
+using System.Text.RegularExpressions;
 public class GameList : MonoBehaviour
 {
     string ip = "localhost";
     [SerializeField] TMP_InputField inputField;
+    Regex regex;
     public void StartHost()
     {
         NetworkManager.singleton.StartHost();
     }
     public void JoinGame()
     {
-        NetworkManager.singleton.networkAddress = ip;
-        NetworkManager.singleton.StartClient();
+        regex = new Regex("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$");
+
+        if (regex.Match(ip).Success)
+        {
+            NetworkManager.singleton.networkAddress = ip;
+            NetworkManager.singleton.StartClient();
+        }
+        else
+            print("Error: Wrong IP " + ip);
+        
     }
     public void JoinWebGame()
     {
         ip = inputField.text;
         JoinGame();
     }
+
 }
