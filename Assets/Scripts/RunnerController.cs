ec18df9db784db588c0b1d6d638a3da6c548a420
Derek Woodard <50028101+djw191@users.noreply.github.com>
2022-01-23 16:48:21 -0600

    jumpfix

--- Assets/Scripts/RunnerController.cs
+++ Assets/Scripts/RunnerController.cs
@@ -5,49 +5,68 @@ using Cinemachine;
 using Mirror;
 public class RunnerController : NetworkBehaviour
 {
-    CinemachineVirtualCamera Vcam;
-    public Rigidbody rb;
-    public float speed;
-    public float jumpForce;
-    float moveY;
-    bool wasGrounded;
+    public float mouseSensitivity = 800f;
+    GameObject Vcam;
+
+    float xRotation = 0f;
+    public CharacterController controller;
+    public float speed = 12f;
+
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask ground;
+    public bool isGrounded;
     void Start()
     {
         if (isLocalPlayer)
         {
-            Vcam = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
-            Vcam.Follow = gameObject.transform;
-            Vcam.LookAt = gameObject.transform;
-            Vcam.GetCinemachineComponent<CinemachineOrbitalTransposer>().m_FollowOffset = new Vector3(0, 0.15f, -0.5f);
-        }
+            Camera.main.transform.SetParent(gameObject.transform);
+            Camera.main.GetComponent<CinemachineBrain>().enabled = false;
+            Camera.main.transform.localPosition = new Vector3(0, 0.8f, 0);
+            Camera.main.transform.localEulerAngles = new Vector3(0,0,0);
+            Vcam = GameObject.Find("CM vcam1");
+            Vcam.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+        }        
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (isLocalPlayer)
         {
-            moveY = Input.GetAxisRaw("Vertical");
-            if (wasGrounded && Input.GetKeyDown(KeyCode.Space))
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, ground);
+            if(isGrounded && velocity.y < 0)
             {
-                //Jump
-                rb.AddForce(Vector3.up * jumpForce);
-                wasGrounded = false;
+                velocity.y = -2f;
             }
-        }
-    }
+            
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            Vector3 move = transform.right * x + transform.forward * z;            
 
-    private void FixedUpdate()
-    {
-        if (isLocalPlayer)
-        {
-            Vector3 camDir = Vcam.transform.forward;
-            rb.velocity = new Vector3(speed * moveY * camDir.x, rb.velocity.y, speed * moveY * camDir.z);
-        }
-    }
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            
+            transform.Rotate(Vector3.up * mouseX);            
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.collider.ClosestPointOnBounds(this.transform.position).y < this.transform.position.y)
-            wasGrounded = true;
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+
+            if (!pauseMenu.singleton.isPaused)
+            {
+                controller.Move(move * speed * Time.deltaTime);
+                Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+                if (Input.GetButtonDown("Jump") && isGrounded)
+                {
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                }
+            }
+        }        
     }
 }
